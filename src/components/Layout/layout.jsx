7572ffe463127f4f134d2df613db40ee5678b982
soxvox <mantle@phobos.phobos>
2025-03-17 23:36:04 +0300

    HW-5 ScrollProgressBar

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -1,8 +1,19 @@
+import { ScrollProgressBar } from "../ScrollProgressBar/scroll-progress-bar";
+
 export const Layout = ({ children, title }) => {
   return (
     <div>
-      <header style={{ position: "fixed" }}>
+      <header
+        style={{
+          position: "fixed",
+          width: "100%",
+          backgroundColor: "silver",
+          top: "0",
+          left: "0",
+        }}
+      >
         <h1>{title}</h1>
+        <ScrollProgressBar />
       </header>
       <section style={{ paddingTop: "100px" }}>{children}</section>
       <footer>footer</footer>
