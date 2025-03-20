d140229a3f3ce8d26bc43653d28329c45435f10d
soxvox <mantle@phobos.phobos>
2025-03-20 23:11:57 +0300

    HW-6 styles to modules

--- src/components/Tabs/tab-list-item.jsx
+++ src/components/Tabs/tab-list-item.jsx
@@ -1,20 +1,14 @@
+import styles from "./tab-list-item.module.css";
+
 export const TabListItem = ({ children, onClick, active }) => {
   return (
     <li
       style={{
-        padding: "5px",
-        marginRight: "1px",
-        border: "1px solid black",
-        display: "inline",
         backgroundColor: active ? "#FFFFFF" : "#DDDDDD",
       }}
+      className={styles.tabListItem}
     >
-      <button
-        onClick={onClick}
-        style={{ border: "none", backgroundColor: "transparent" }}
-      >
-        {children}
-      </button>
+      <button onClick={onClick}>{children}</button>
     </li>
   );
 };
